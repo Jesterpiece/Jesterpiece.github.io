55ed7f1e1a3472b8bed54ff5e5dda72425796558
Jesterpiece <132614452+Jesterpiece@users.noreply.github.com>
2023-05-10 08:43:35 +0200

    Add files via upload
    Added workers

--- script.js
+++ script.js
@@ -129,3 +129,82 @@ svg.addEventListener('click', function() {
     svg.classList.remove('animate');
   }, 100);
 });
+
+// WORKERS!!! Remember that, retard!
+
+class Worker {
+  constructor(name, baseCost, baseHarvest, baseSpeed) {
+    this.name = name;
+    this.baseCost = baseCost;
+    this.baseHarvest = baseHarvest;
+    this.baseSpeed = baseSpeed;
+    this.cost = baseCost;
+    this.level = 0;
+    this.harvest = baseHarvest;
+    this.speed = baseSpeed;
+    this.progress = 0;
+    this.maxProgress = 100;
+    this.timerId = null;
+    this.buyButton = document.getElementById(`${this.name}-buy`);
+    this.progressBar = document.getElementById(`${this.name}-progress-bar`);
+    this.levelDisplay = document.getElementById(`${this.name}-lvl`);
+    this.costDisplay = document.getElementById(`${this.name}-cost`);
+    this.harvestDisplay = document.getElementById(`${this.name}-harvest`);
+    this.buyButton.addEventListener("click", () => this.buy());
+    this.updateDisplay();
+  }
+
+  buy() {
+    if (cash >= this.cost) {
+      cash -= this.cost;
+      this.level++;
+      this.updateHarvest();
+      this.cost = Math.ceil(this.cost * 1.2);
+      this.updateDisplay();
+      this.startWorking();
+      cashEl.innerText = cash.toFixed(2);
+    }
+  }
+
+  updateHarvest() {
+    this.harvest = this.level * this.baseHarvest;
+    this.updateDisplay();
+  }
+
+  startWorking() {
+    if (this.progress === 0) {
+      this.timerId = setInterval(() => this.updateProgress(), this.speed * 10);
+    }
+  }
+
+  updateProgress() {
+    this.progress++;
+    if (this.progress >= this.maxProgress) {
+      this.finishWork();
+    } else {
+      this.updateProgressBar();
+    }
+  }
+
+  finishWork() {
+    this.progress = 0;
+    kgOnions += this.harvest;
+    kgOnionsEl.innerText = kgOnions;
+    sellBtn.disabled = false;
+    clearInterval(this.timerId);
+    this.startWorking();
+  }
+
+  updateProgressBar() {
+    const percent = (this.progress / this.maxProgress) * 100;
+    this.progressBar.style.width = `${percent}%`;
+  }
+
+  updateDisplay() {
+    this.levelDisplay.innerText = this.level;
+    this.costDisplay.innerText = this.cost;
+    this.harvestDisplay.innerText = this.harvest;
+  }
+}
+const monkeyWorker = new Worker("monkey", 6, 2, 5);
+const clownWorker = new Worker("clown", 6, 20, 8);
